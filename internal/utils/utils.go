c91368d9dc69cf7ea7c9224a5a9332fa56971ae0
Andreas Jönsson <adde90@gmail.com>
2024-06-05 01:41:20 +0200

    Add new util func to generate unique IDs

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,3 +1,17 @@
 package utils
 
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
 func Int32Ptr(i int32) *int32 { return &i }
+
+func GenerateUniqueID() string {
+	randomBytes := make([]byte, 8)
+	if _, err := rand.Read(randomBytes); err != nil {
+		panic(err)
+	}
+
+	return hex.EncodeToString(randomBytes)
+}
